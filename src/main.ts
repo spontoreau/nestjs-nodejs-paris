2ca16d8d04f20f4d63cf9fe0cca25feddb64b66b
spontoreau <pontoreau.sylvain@gmail.com>
2019-10-09 14:05:32 +0200

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; 
 import { config } from 'dotenv';
 
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
+  const options = new DocumentBuilder() 
+  .setTitle('User API') 
+  .setDescription('The user API description') 
+  .setVersion('1.0') 
+  .addTag('user')
+  .build(); 
+
+const document = SwaggerModule.createDocument(app, options);     
+SwaggerModule.setup('api', app, document); 
+
   await app.listen(3000);
 }
 bootstrap();
